280f5aa70785ceadc86bfe9f8b2f35f1ba05845a
MattDevy <mattd905@gmail.com>
2020-07-20 03:55:53 +0100

    Added paper and spacing to home page

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Code = () => {
-    return (
-        <div className="code">
-            Code
-        </div>
-    )
-}
+  return <div className="code">Code</div>;
+};
 
-export default Code
+export default Code;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,59 @@
-import React from 'react'
+import React from "react";
 
-import { Container, Paper, Typography } from '@material-ui/core'
+import { Container, Paper, Typography, Divider } from "@material-ui/core";
+
+import {
+  Theme,
+  useTheme,
+  makeStyles,
+  createStyles,
+} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {},
+    paper: {
+      width: "100%",
+      padding: theme.spacing(3),
+      margin: theme.spacing(3),
+    },
+    header: {
+      margin: theme.spacing(3),
+    },
+    divider: {
+      margin: theme.spacing(2),
+    },
+    body: {
+      margin: theme.spacing(3),
+    },
+  })
+);
 
 const Home = () => {
-    return (
-        <Container className="Home" maxWidth="lg">
-            <Paper>
-                Hello
-            </Paper>
-        </Container>
-    )
-}
+  const classes = useStyles();
+  const theme = useTheme();
+  return (
+    <Container className={classes.root} maxWidth="lg">
+      <Paper className={classes.paper}>
+        <Typography variant="h1" className={classes.header}>
+          Welcome
+        </Typography>
+        {/* <Divider className={classes.divider}/> */}
+      </Paper>
+      <Paper className={classes.paper}>
+        <Typography variant="h3" className={classes.header}>
+          Introduction
+        </Typography>
+        <Typography variant="body1" className={classes.body}>
+          Welcome to my site. Where you can find my latest:
+          <ul>
+            <li>Personal projects</li>
+            <li>Development interests</li>
+          </ul>
+        </Typography>
+      </Paper>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
